b9781270da8961ff05055063efcdd8fcc3890d78
adesduende <sergiolopezlobo1990@gmail.com>
2024-06-24 21:37:45 +0200

    Add text draw

--- PieChart.js
+++ PieChart.js
@@ -5,19 +5,26 @@ export default class PieChart {
     #centerPoint;
     #radius;
 
+    //Canvas properties
     backgroundColor = "transparent";
     canvasOffset = 0;
     border = 0;
-    pieOffset = 10;
+    pieOffset = 0;
     showName = true;
     showValue = true;
+
+    //Pie properties
+    foregroundColor = "#000000";
+    textFamily = "Arial Black";
+    textSize = 10;
+    textSpan = 2;
 
     constructor(canvasId) {
         this.#canvasCtx = document.getElementById(canvasId).getContext('2d');
         this.#canvasWidth = document.getElementById(canvasId).width;
         this.#canvasHeight = document.getElementById(canvasId).height;
         this.#centerPoint = { x: this.#canvasWidth/2, y: this.#canvasHeight/2 };
-        this.#radius = (this.#canvasWidth<this.#canvasHeight)?this.#canvasWidth/2:this.#canvasHeight/2;
+        this.#radius = (this.#canvasWidth<this.#canvasHeight)?this.#canvasWidth/2:this.#canvasHeight/2;        
     }
 
     DrawGraph(data) {
@@ -25,6 +32,9 @@ export default class PieChart {
         this.#canvasCtx.clearRect(0, 0, this.#canvasWidth, this.#canvasHeight);
         this.#canvasCtx.fillStyle = this.backgroundColor;
         this.#canvasCtx.fillRect(0, 0, this.#canvasWidth, this.#canvasHeight);
+
+        //Set font properties
+        this.#canvasCtx.font = `${this.textSize}px ${this.textFamily}`;
 
         //Get max value
         const maxValue = this.#GetMaxValue(data);
@@ -38,22 +48,42 @@ export default class PieChart {
             const endAngle = initAngle + this.#GetProportionalAngle(element.value, totalValue);
             this.#DrawPie(initAngle,endAngle,element.color);
             initAngle = endAngle;
+
+            //Draw name
+            if(this.showName)
+            {
+                const textAngle = initAngle - this.#GetProportionalAngle(element.value, totalValue)/2;
+                const x = this.#centerPoint.x + this.#radius/2 * Math.cos(textAngle);
+                const y = this.#centerPoint.y + this.#radius/2 * Math.sin(textAngle);
+                this.#DrawText(x, y, element.name, this.foregroundColor);
+            }
+            if(this.showValue)
+            {
+                const textDimension = this.#GetTextDimensions(element.value);
+                const textAngle = initAngle - this.#GetProportionalAngle(element.value, totalValue)/2;
+                const x = this.#centerPoint.x + this.#radius/2 * Math.cos(textAngle);
+                const y = this.#centerPoint.y + this.#radius/2 * Math.sin(textAngle) + textDimension.height + this.textSpan;
+                this.#DrawText(x, y, element.value, this.foregroundColor);
+            }
         });
-
-        
-        
     }
-
+    ///DrawPie
+    ///@param {number} initAngle: initial angle in radians
+    ///@param {number} endAngle: end angle in radians
+    ///@param {string} color: color of the pie
     #DrawPie(initAngle, endAngle, color) {
 
-        const x = this.#centerPoint.x + this.pieOffset;
-        const y = this.#centerPoint.y;
+        //Set span pie
+        const textAngle = initAngle + (endAngle - initAngle)/2;
+
+        const x = this.#centerPoint.x + this.pieOffset * Math.cos(textAngle);
+        const y = this.#centerPoint.y + this.pieOffset * Math.sin(textAngle);
         const radius = this.#radius-this.canvasOffset;
 
         this.#canvasCtx.save();
         this.#canvasCtx.fillStyle = color;
         this.#canvasCtx.beginPath();
-        this.#canvasCtx.moveTo(x, x);
+        this.#canvasCtx.moveTo(x, y);
         this.#canvasCtx.arc(x, y,radius, initAngle , endAngle , false);
         this.#canvasCtx.closePath();
         this.#canvasCtx.lineWidth = this.border;
@@ -61,8 +91,7 @@ export default class PieChart {
         if(this.border>0)
             this.#canvasCtx.stroke();
         this.#canvasCtx.restore();
-    }
-    
+    }    
     ///Degree to Radians
     ///@param {number} degree value in degrees
     ///@returns {number} value in radians
@@ -100,5 +129,25 @@ export default class PieChart {
         });
 
         return totalValue;
+    }
+    ///DrawText
+    ///@param {number} x: x position
+    ///@param {number} y: y position
+    ///@param {string} name: name of the element
+    ///@param {number} value: value of the element
+    ///@param {string} color: color of the element
+    #DrawText(x, y, name, color) {
+        this.#canvasCtx.save();
+        this.#canvasCtx.fillStyle = color;
+        this.#canvasCtx.fillText(name, x, y);
+        this.#canvasCtx.restore();
+    }
+
+    ///GetTextDimensions
+    ///@param {string} text: text to get the dimensions
+    ///@returns {object} width and height of the text
+    #GetTextDimensions(text) {
+        const dimensions = this.#canvasCtx.measureText(text);
+        return { width: dimensions.width, height: this.textSize };
     }
 }
